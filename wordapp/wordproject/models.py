5d6295184daffa8d9327c5220b59c2a50376bc1d
tsgardiner <tsgardiner@windowslive.com>
2016-03-31 11:23:17 +1300

    Model and Admin created

--- wordapp/wordproject/models.py
+++ wordapp/wordproject/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class WordRecord(models.Model):
+    englishWord = models.CharField(max_length=100)
+    maoriWord = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True, max_length=200)
+    dateCreate = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now=True)
+    publish = models.BooleanField(default=False)
